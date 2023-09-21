846d74ccf6a9af7fe5b49516728b3c61e7a62ff5
Jafar-ibrahim <jafarfarwana@gmail.com>
2023-09-21 16:50:17 +0300

    Created Config/StartUpApp.java

---
+++ src/main/java/com/example/CinemaApp/Config/StartUpApp.java
@@ -0,0 +1,14 @@
+package com.example.CinemaApp.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StartUpApp implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}

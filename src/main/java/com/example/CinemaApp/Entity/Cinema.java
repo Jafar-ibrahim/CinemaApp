f0c1d445cc8fcba13f3339a97a35f8c677edf2d0
Jafar-ibrahim <jafarfarwana@gmail.com>
2023-09-21 16:49:12 +0300

    Added "name" field

--- src/main/java/com/example/CinemaApp/Entity/Cinema.java
+++ src/main/java/com/example/CinemaApp/Entity/Cinema.java
@@ -20,6 +20,8 @@ public class Cinema {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
     @OneToMany(mappedBy = "cinema")
     private List<Theater> theaters;
 }

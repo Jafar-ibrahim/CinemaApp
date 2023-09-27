a7e4e4243073f08a858810e16995c792343e06b0
Jafar-ibrahim <jafarfarwana@gmail.com>
2023-09-28 00:41:37 +0300

    created repositories

--- src/main/java/com/example/CinemaApp/Repository/TicketRepo.java
+++ src/main/java/com/example/CinemaApp/Repository/TicketRepo.java
@@ -2,6 +2,8 @@ package com.example.CinemaApp.Repository;
 
 import com.example.CinemaApp.Entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,14 @@ public interface TicketRepo extends JpaRepository<Ticket,Long> {
     List<Ticket> findAllByReservedFalse();
 
     Optional<Ticket> findById(Long id);
+
+    Optional<Ticket> findByRowNoAndColumnNoAndTheater_Id(int row , int col , Long theaterId);
+
+    @Query(value = "SELECT SUM(price) FROM tickets WHERE movie_name = :movieName", nativeQuery = true)
+    double sumPriceByMovieName(@Param("movieName") String movieName);
+
+    @Query(value = "SELECT SUM(price) FROM tickets WHERE DATE(date_of_purchase) = CURRENT_DATE AND movie_name = :movieName", nativeQuery = true)
+    Double sumPriceByMovieNameAndToday(@Param("movieName") String movieName);
+
+
 }
--- src/main/java/com/example/CinemaApp/Repository/UserRepo.java
+++ src/main/java/com/example/CinemaApp/Repository/UserRepo.java
@@ -2,9 +2,12 @@ package com.example.CinemaApp.Repository;
 
 import com.example.CinemaApp.Entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends JpaRepository<AppUser,Long> {
     Optional<AppUser> findByEmail(String email);
+    Optional<AppUser> findById(Long id);
 }
